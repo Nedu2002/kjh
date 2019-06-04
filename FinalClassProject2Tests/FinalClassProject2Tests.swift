0c3031d50b482bea9e854a419315289eac34b11f
Chinedu <acon212000@yahoo.com>
2019-06-04 18:03:32 -0400

    Initial Commit

---
+++ FinalClassProject2Tests/FinalClassProject2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  FinalClassProject2Tests.swift
+//  FinalClassProject2Tests
+//
+//  Created by Chinedu Nmezi on 2019-06-04.
+//  Copyright © 2019 Chinedu Nmezi. All rights reserved.
+//
+
+import XCTest
+@testable import FinalClassProject2
+
+class FinalClassProject2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
